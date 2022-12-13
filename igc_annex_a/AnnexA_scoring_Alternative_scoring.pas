84febdf8a22298662a3e2a47e885dadf568b68de
Ján Hrnčírik <jan.hrncirik@gmail.com>
2022-12-13 13:34:19 +0100

    Changing the calculation HHF := CountFixes + HHF;

--- igc_annex_a/AnnexA_scoring_Alternative_scoring.pas
+++ igc_annex_a/AnnexA_scoring_Alternative_scoring.pas
@@ -306,7 +306,8 @@ begin
            PilotStartSpeed := (Pilots[i].Fixes[HHF].Gsp + Pilots[i].Fixes[HHF+1].Gsp)/2;
           end 
           else
-           HHF := Trunc(3*HHF / 2);
+           HHF := CountFixes + HHF;
+           if Trunc(HHF/2) > CountFixes-1 then StartFixFound := false;
           end;  
          if HHF < 3 then StartFixFound := false;     
         end;
