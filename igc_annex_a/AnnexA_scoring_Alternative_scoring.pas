ecef9797af7a5d15a34975d188067cbfc463ad37
Ján Hrnčírik <jan.hrncirik@gmail.com>
2022-12-16 14:44:15 +0100

    Debugigng 2

--- igc_annex_a/AnnexA_scoring_Alternative_scoring.pas
+++ igc_annex_a/AnnexA_scoring_Alternative_scoring.pas
@@ -298,6 +298,7 @@ begin
             Info1 := 'element out of scope item = ' + IntToStr(item);
             exit;
           end;
+          
           while (Vleft <= Vright) do begin // if we have something to share
             center:=(Vleft + Vright) div 2;
             if (item = Pilots[i].Fixes[center].Tsec) then
@@ -312,7 +313,7 @@ begin
               Vright:=center - 1 // throw away the Vright half
             else
               Vleft:=center + 1; // discard the Vleft half
-              if (item < Pilots[i].Fixes[center+1].Tsec) then
+              if (item < Pilots[i].Fixes[Vleft].Tsec) then
                 begin
                   PilotStartAlt := (Pilots[i].Fixes[center].AltQnh + Pilots[i].Fixes[center+1].AltQnh) / 2;
                   PilotStartSpeed := (Pilots[i].Fixes[center].Gsp + Pilots[i].Fixes[center+1].Gsp) / 2;
